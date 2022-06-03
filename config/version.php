8551190dc61e0c0e4a2f953b5ea221eb1b168f93
Aleem A <aleem.333@hotmail.com>
2022-06-03 18:32:49 +0500

    Changes

---
+++ config/version.php
@@ -0,0 +1,17 @@
+<?php
+
+$hash = trim(exec('git log --pretty="%h" -n1 HEAD'));
+$date = Carbon\Carbon::parse(trim(exec('git log -n1 --pretty=%ci HEAD')));
+
+$tag  = exec('git describe');
+$tag  = str_replace('-g' . $hash, '', $tag);
+
+$string = $date->format('m-d-Y H:i:s') . " ($tag)";
+
+return [
+    'tag'    => $tag,
+    'date'   => $date,
+    'hash'   => $hash,
+    'string' => $string,
+];
+
